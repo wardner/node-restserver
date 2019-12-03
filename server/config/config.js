7242cbbc1934fd0d49e2c8747997ffaa06e47262
Wardner <wanerlara1999@gmail.com>
2019-12-02 23:18:48 -0400

    BD Connection Fixed

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================
 let urlDB;
 
-if (process.env.NODE_ENV) {
+
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://wardner:gKeic26akkKdMBSr@cluster0-ixeyl.mongodb.net/cafe';
